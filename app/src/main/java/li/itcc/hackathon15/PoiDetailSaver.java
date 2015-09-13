6da3e6a01d9111a1b7730c44751385578de2b1fb
Arthur Marxer <arthur.marxer@gmail.com>
2015-09-13 13:50:14 +0200

    Add capture image

--- app/src/main/java/li/itcc/hackathon15/PoiDetailSaver.java
+++ app/src/main/java/li/itcc/hackathon15/PoiDetailSaver.java
@@ -1,7 +1,10 @@
 package li.itcc.hackathon15;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
+
+import java.io.ByteArrayOutputStream;
 
 import li.itcc.hackathon15.services.PoiDetailBean;
 import li.itcc.hackathon15.services.PoiServices;
@@ -22,17 +25,31 @@ public class PoiDetailSaver {
         fListener = listener;
     }
 
-    public void save(PoiDetailBean bean) {
-        new SaveTask().execute(bean);
+    public void save(PoiDetailBean bean, Bitmap bitmap) {
+        new SaveTask(bitmap).execute(bean);
     }
 
     private class SaveTask extends AsyncTask<PoiDetailBean, Void, Void> {
+        private final Bitmap fBitmap;
         private Throwable fException;
+
+        public SaveTask(Bitmap bitmap) {
+            fBitmap = bitmap;
+        }
 
 
         @Override
         protected Void doInBackground(PoiDetailBean... params) {
             try {
+                PoiDetailBean param = params[0];
+                if (fBitmap != null) {
+                    // compress bitmap to jpeg in background
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    fBitmap.compress(Bitmap.CompressFormat.JPEG, 80, out);
+                    out.flush();
+                    out.close();
+                    param.setImage(out.toByteArray());
+                }
                 PoiServices poiServices = new PoiServices(fContext, TitleHolder.PoiConstants.URL);
                 poiServices.savePoiDetails(params[0]);
             }
